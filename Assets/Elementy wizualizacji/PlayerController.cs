6ece5f162fe8c7868ab1fbfe4630f09af894d7a0
Robert Ostrowski <robbostrowski@gmail.com>
2018-03-30 14:41:00 +0200

    Init commit
    Działaj pls

---
+++ Assets/Elementy wizualizacji/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 70;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        float throttle = Input.GetAxis("Vertical");
+        float rotate = Input.GetAxis("Horizontal");
+        
+        GetComponent<Rigidbody>().AddForce(transform.forward * throttle * Time.deltaTime * speed * this.transform.localScale.x);
+        GetComponent<Rigidbody>().AddTorque(transform.up * rotate * Time.deltaTime * speed );
+    }
+}
